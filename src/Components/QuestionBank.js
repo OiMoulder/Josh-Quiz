1326f907b5ac4ef319bf5ec66cb02825f8ba479f
Josh Moulder <56400569+OiMoulder@users.noreply.github.com>
2024-04-15 02:08:54 +0000

    changes

---
+++ src/Components/QuestionBank.js
@@ -0,0 +1,36 @@
+// QuestionBank.js
+
+const qBank = [
+  {
+    id: 1,
+    question: "What is the capital of England?",
+    options: ["London", "Manchester", "Liverpool", "Oxford"],
+    answer: "London",
+  },
+  {
+    id: 2,
+    question: "What is a town in Essex?",
+    options: ["Dartford", "Great Yarmouth", "Chelmsford", "Tonbridge"],
+    answer: "Chelmsford",
+  },
+  {
+    id: 3,
+    question: "What is a car brand?",
+    options: ["Amazon", "Doritos", "Starbucks", "Ford"],
+    answer: "Ford",
+  },
+  {
+    id: 4,
+    question: "What is the first number?",
+    options: ["One", "Two", "Three", "Four"],
+    answer: "One",
+  },
+  {
+    id: 5,
+    question: "What is 5 minus 1?",
+    options: ["1", "2", "3", "4"],
+    answer: "4",
+  },
+];
+
+export default qBank;
